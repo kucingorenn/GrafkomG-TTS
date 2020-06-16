425dd9cbeb405d6aafa732dbee49780167fad18d
kucingorenn <65481065+kucingorenn@users.noreply.github.com>
2020-06-17 00:06:43 +0700

    tambah motif dasar hitam

--- GrafkomG-TTS/Source.cpp
+++ GrafkomG-TTS/Source.cpp
@@ -76,7 +76,116 @@ void display() {
 	glVertex2f(0, 140);
 	glEnd();
 
+	glBegin(GL_POLYGON);
+	glVertex2f(50, 210);
+	glVertex2f(120, 210);
+	glVertex2f(120, 500);
+	glVertex2f(50, 500);
+	glEnd();
 
+	glBegin(GL_POLYGON);
+	glVertex2f(50, 390);
+	glVertex2f(50, 460);
+	glVertex2f(0, 460);
+	glVertex2f(0, 390);
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	glVertex2f(40, 470);
+	glVertex2f(40, 550);
+	glVertex2f(-40, 470);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(40, 220);
+	glVertex2f(40, 380);
+	glVertex2f(0, 380);
+	glVertex2f(0, 220);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(130, 500);
+	glVertex2f(200, 500);
+	glVertex2f(200, 380);
+	glVertex2f(130, 380);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(200, 380);
+	glVertex2f(200, 450);
+	glVertex2f(500, 450);
+	glVertex2f(500, 380);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex2f(290, 500);
+	glVertex2f(360, 500);
+	glVertex2f(360, 450);
+	glVertex2f(290, 450);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(210, 500);
+	glVertex2f(280, 500);
+	glVertex2f(280, 460);
+	glVertex2f(210, 460);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(370, 500);
+	glVertex2f(500, 500);
+	glVertex2f(500, 460);
+	glVertex2f(370, 460);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(370, 140);
+	glVertex2f(370, 370);
+	glVertex2f(440, 370);
+	glVertex2f(440, 140);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(440, 370);
+	glVertex2f(440, 300);
+	glVertex2f(500, 300);
+	glVertex2f(500, 370);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(450, 300);
+	glVertex2f(520, 300);
+	glVertex2f(520, 220);
+	glVertex2f(450, 220);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(440, 210);
+	glVertex2f(500, 210);
+	glVertex2f(500, 140);
+	glVertex2f(440, 140);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(370, 0);
+	glVertex2f(440, 0);
+	glVertex2f(440, 130);
+	glVertex2f(370, 130);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(440, 130);
+	glVertex2f(440, 60);
+	glVertex2f(500, 60);
+	glVertex2f(500, 130);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2f(450, 50);
+	glVertex2f(500, 50);
+	glVertex2f(500, -20);
+	glVertex2f(450, -20);
+	glEnd();
 
 	glFlush();
 }
@@ -87,7 +196,7 @@ void myinit() {
 	glPointSize(2.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0, 1000, 1000, 0);
+	gluOrtho2D(0, 500, 500, 0);
 }
 
 int main(int argc, char** argv) {
